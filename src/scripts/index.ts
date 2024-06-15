7d867348df2dc27726aced9d98423ee35ab63525
Julio Rahman <julio.rahman@gmail.com>
2024-06-15 09:13:57 +0000

    chore: update source path in index.ts

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,7 +2,7 @@ import { Rive } from "@rive-app/canvas-lite";
 
 document.addEventListener("astro:page-load", () => {
   const r = new Rive({
-    src: "../scroll_down.riv",
+    src: "../../scroll_down.riv",
     canvas: document.getElementById(
       "scroll-down-canvas",
     ) as HTMLCanvasElement,
