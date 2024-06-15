e1ce6aca0781e8a853d0c1a70b8e2c45c3886b19
Julio Rahman <julio.rahman@gmail.com>
2024-06-15 11:03:44 +0000

    chore: implement hotjar

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,4 +1,10 @@
+import Hotjar from '@hotjar/browser';
 import { Rive } from "@rive-app/canvas-lite";
+
+const siteId = 5025471;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 
 document.addEventListener("astro:page-load", () => {
   const r = new Rive({
