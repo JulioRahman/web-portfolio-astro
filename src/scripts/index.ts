88569626ec421a1df6792bf2a8186d23518b2727
Julio Rahman <julio.rahman@gmail.com>
2024-07-07 12:02:06 +0000

    refactor: update carousel component and styles

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,5 +1,8 @@
 import Hotjar from '@hotjar/browser';
 import { Rive } from "@rive-app/canvas-lite";
+import EmblaCarousel, { type EmblaOptionsType } from "embla-carousel";
+import Autoplay, { type AutoplayOptionsType } from "embla-carousel-autoplay";
+import { addDotBtnsAndClickHandlers } from './EmblaCarouselDotButton'
 
 const siteId = 5025471;
 const hotjarVersion = 6;
@@ -16,5 +19,22 @@ document.addEventListener("astro:page-load", () => {
     onLoad: () => {
       r.resizeDrawingSurfaceToCanvas();
     },
+  });
+
+  const emblaNodes = [].slice.call(document.querySelectorAll(".embla"));
+  const dotsNodes = [].slice.call(document.querySelectorAll(".embla__dots"));
+  const emblaOptions: EmblaOptionsType = { loop: true };
+  const autoplayOptions: AutoplayOptionsType = { delay: 5000 };
+
+  const emblaCarousels = emblaNodes.map(function (emblaNode) {
+    return EmblaCarousel(emblaNode, emblaOptions, [
+      Autoplay(autoplayOptions),
+    ]);
+  });
+
+
+  dotsNodes.forEach((node, i) => {
+    const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(emblaCarousels[i], node);
+    emblaCarousels[i].on('destroy', removeDotBtnsAndClickHandlers)
   });
 });
